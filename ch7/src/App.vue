6924429ba003a41082c8e88b0ce54979d1691ec8
tsu-nera <fox10225fox@gmail.com>
2018-12-29 02:30:29 +0900

    いったんcommit

--- ch7/src/App.vue
+++ ch7/src/App.vue
@@ -3,18 +3,77 @@
   <h2>タスク一覧</h2>
   <ul>
     <li v-for="task in tasks" :key="task.id">
-      <input type="checkbox" :checked="task.done">
+      <input type="checkbox" :checked="task.done" @change="toogleTaskStatus(task)" />
       {{ task.name }}
+
+      <span v-for="id in task.labelIds" :key='id'>
+        {{ getLabelText(id) }}
+      </span>
     </li>
   </ul>
+
+  <form @submit.prevent="addTask">
+    <input type="text" v-model="newTaskName" placeholder="新しいタスク">
+  </form>
+
+  <h2>ラベル一覧</h2>
+  <ul>
+    <li v-for="label in labels" :key="label.id">
+      <input type="checkbox" :value="label.id" v-model="newTaskLabelIds" >
+      {{ label.text }}
+    </li>
+  </ul>
+
+  <form @submit.prevent="addLabel">
+    <input type="text" v-model="newLabelText" placeholder="新しいラベル">
+  </form>
 </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      newTaskName: '',
+      newTaskLabelIds: [],
+      newLabelText: ''
+    }
+  },
   computed: {
     tasks () {
       return this.$store.state.tasks
+    },
+    labels () {
+      return this.$store.state.labels
+    }
+  },
+
+  methods: {
+    addTask () {
+      this.$store.commit('addTask', {
+        name: this.newTaskName,
+        labelIds: this.newTaskLabelIds
+      })
+      this.newTaskName = ''
+      this.newTaskLabelIds = []
+    },
+
+    toggleTaskStatus (task) {
+      this.$store.commit('toggleTaskStatus', {
+        id: task.id
+      })
+    },
+
+    addLabel () {
+      this.$store.commit('toggleTaskStatus', {
+        text: this.newLabelText
+      }),
+      this.newLabelText = ''
+    },
+
+    getLabelText (id) {
+      const label = this.labels.filter(label => label.id === id)[0]
+      return label ? label.text : ''
     }
   }
 }
--- ch7/src/store.js
+++ ch7/src/store.js
@@ -9,15 +9,64 @@ const store = new Vuex.Store({
       {
         id: 1,
         name: '牛乳を買う',
+        labelIds: [1, 2],
         done: false
       },
       {
         id: 2,
         name: 'Vue.jsの本を買う',
+        labelIds: [1, 3],
         done: true
-      }
-    ]
-  }
+      },
+    ],
+
+    labels: [
+      {
+        id: 1,
+        text: '買い物'
+      },
+      {
+        id: 2,
+        text: '食料',
+      },
+      {
+        id: 3,
+        text: '本'
+      },
+    ],
+    nextTasksId: 3,
+    nextLabelId: 4,
+  },
+
+  mutations: {
+    addTask (state, { name, labelIds } ) {
+      state.tasks.push({
+        id: state.nextTasksId,
+        name,
+        labelIds,
+        done: false
+      })
+      state.nextTasksId++
+    },
+
+    toggleTaskStatus (state, { id }) {
+      const filtered = state.tasks.filter(task => {
+        return task.id === id
+      })
+
+      filtered.forEach(task => {
+        task.done = !task.done
+      })
+    },
+
+    addLabel (state, { text }) {
+      state.labels.push({
+        id: state.nextLabelId,
+        text
+      })
+      state.nextLabelId++;
+    }
+  }  
 })
 
 export default store
